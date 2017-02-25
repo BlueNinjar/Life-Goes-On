e24776b672d0f2ec0cb9b33b21a230edb06b7232
Luke Paul <lukepaultech@gmail.com>
2017-02-25 23:56:12 +0000

    Added:Texture & Smelting for Tust Ore

--- src/main/java/com/imbluesmedia/lifegoeson/recipe/LGORecipe.java
+++ src/main/java/com/imbluesmedia/lifegoeson/recipe/LGORecipe.java
@@ -1,5 +1,7 @@
 package com.imbluesmedia.lifegoeson.recipe;
 
+import com.imbluesmedia.lifegoeson.LifeGoesOn;
+import com.imbluesmedia.lifegoeson.block.LGOBlock;
 import com.imbluesmedia.lifegoeson.item.LGOItems;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
@@ -17,6 +19,7 @@ public class LGORecipe {
         GameRegistry.addShapedRecipe(new ItemStack(LGOItems.lgoSpeedStone), "SIS", "IDI", "SIS", 'S', Items.SUGAR, 'I', Items.IRON_INGOT, 'D', Items.DIAMOND);
         //Fire Stone
         GameRegistry.addShapedRecipe(new ItemStack(LGOItems.lgoFlameStone), "LOL", "ODO", "LOL", 'L', Items.LAVA_BUCKET, 'O', Blocks.OBSIDIAN, 'D', Items.DIAMOND);
-
+        //Tusk Ore
+        GameRegistry.addSmelting(LGOBlock.lgoTuskOre, new ItemStack(Items.DIAMOND, 2), 1F);
     }
 }
